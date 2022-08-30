fdbb12942cf8d73920c25565ad140d7f852cefc7
smk <sudhanshumohan781@gmail.com>
2022-08-30 21:58:34 +0530

    English Translation and Config Setting

--- init.lua
+++ init.lua
@@ -1,21 +1,23 @@
 local death_cetro = {}
-local give_on_death = true
+local give_on_death = minetest.settings:get_bool('death_cetro.give_on_death',true)
 
---Almacenar (Temporalmente) la ubicacion de muerte del jugador
+-- TODO : Add a stack based undo feature upto some limit
+
+--Store (Temporarily) the player's death location
 minetest.register_on_dieplayer(function(player)
     death_cetro[player:get_player_name()] = player:get_pos()
 end)
 
---Dar el cristal cuando el player muere (si give_on_death es verdadero)
-minetest.register_on_respawnplayer(function(player) 
+--Give crystal when player dies (if give_on_death is true)
+minetest.register_on_respawnplayer(function(player)
     if give_on_death and death_cetro[player:get_player_name()]then
         inv = player:get_inventory()
         inv:add_item("main","death_cetro:cetro")
-    end 
+    end
 end)
 
 minetest.register_craftitem("death_cetro:cetro", {
-	description = "".. minetest.colorize("yellow", "cetro del retorno."),
+	description = "".. minetest.colorize("yellow", "scepter of return."),
 	wield_scale = {x=0.75,y=0.75,z=0.75},
 	inventory_image = "resurection_wand.png",
     stack_max = 1,
@@ -27,12 +29,13 @@ minetest.register_craftitem("death_cetro:cetro", {
             death_cetro[user:get_player_name()] = nil
             itemstack:take_item()
         else
-            minetest.chat_send_player(user:get_player_name(),"No has muerto recientemente, ¡felicidades!")
+            minetest.chat_send_player(user:get_player_name(),"You haven't died recently, congratulations!")
         end
         return itemstack
     end
 })
 
+-- if wand is not given on death then make it craftable
 if not give_on_death then
     minetest.register_craft({
 	    output = "death_cetro:cetro",
@@ -42,7 +45,4 @@ if not give_on_death then
 			    {"", "default:gold_ingot", ""}
 		     }
     })
-else
-
-
 end
